0f895545a6eccc3280a09c7d1605df87ff12bef0
root <ejimovc@gmail.com>
2024-09-29 18:28:01 +0100

    Upload

--- utils/redis.js
+++ utils/redis.js
@@ -3,33 +3,36 @@ import { Promisify } from 'utils'
 
 class RedisClient {
     constructor(client) {
-	this.client = redis.createClient;
-	client.on('error', (err) => {
-	    console.log('An error Occured, Redis wom start', err);
-	});
-	client.on('connect', () => {
-	    console.log('Redis connected to server');
-	});
-	this.getAsync = Promisify(this.client.get).bind(this.client);
+        this.client = createClient();
+        client.on('error', (err) => {
+            console.log('An error Occured, Redis wom start', err);
+        });
+        client.on('connect', () => {
+            console.log('Redis connected to server');
+        });
+        this.getAsync = Promisify(this.client.get).bind(this.client);
         this.setAsync = Promisify(this.client.set).bind(this.client);
         this.delAsync = Promisify(this.client.del).bind(this.client);
     };
     isAlive() {
-        if (err) {
-	    return false;
-	}
-	return true;
-	return this.client.connected;
+
+        if (this.client.connected) {
+            return true;
+        }
+        return false
+
     }
+
     async get(key) {
         return await this.getAsync(key);
     }
     async set(key, value, duration) {
-	return await this.setAsync(key, value, 'EX', duratìon);
+        return await this.setAsync(key, value, 'EX', duration);
     }
     async del(key) {
-	await this.delAsync(key);
+        await this.delAsync(key);
     }
-    const redisClient = new RedisClient();
+
 }
+const redisClient = new RedisClient();
 module.exports = redisClient;
