0795d505e246b7d5ed7a36da1ef0d3d227e8b21b
root <ejimovc@gmail.com>
2024-09-30 22:58:06 +0100

    Push

--- utils/db.js
+++ utils/db.js
@@ -2,29 +2,29 @@ import dotenv from 'dotenv';
 import { mongoClient } from 'mongodb'
 
 class DBClient {
-    constructor() {
-	const host = process.env.DB_HOST || 'localhost';
-	const port = process.env.DB_PORT || 'port';
-	const data_base = process.env.DB_DATABASE || 'files_manager';
-	const url = 'mongodb://${host}:${port}';
-	
-	this.client = new mongoClient(url, { useUnifieldTopology: true });
-	this.database = this.client.db(database);
-	this.client.connect().catch((err) => {
-	    console.log('Failed to connect to Mongodb server', err);
-	});
-    };
-    isAlive() => {
-	return this.client && this.client.isconnected();
-    }
+	constructor() {
+		const host = process.env.DB_HOST || 'localhost';
+		const port = process.env.DB_PORT || 'port';
+		const data_base = process.env.DB_DATABASE || 'files_manager';
+		const url = `mongodb://${host}:${port}`;
+
+		this.client = new mongoClient(url, { useUnifieldTopology: true });
+		this.database = this.client.db(database);
+		this.client.connect().catch((err) => {
+			console.log('Failed to connect to Mongodb server', err);
+		});
+	};
+	isAlive(){
+		return this.client && this.client.isconnected();
+	}
     async nbUsers() {
 	const users_collection = this.database.collection('users');
 	return users_collection.countDocuments();
-    }
+}
     async nbFiles() {
 	const files_collection = this.database.collection('files');
 	return files_collection.countDocuments();
-    }
+}
 }
 const dbclient = new DBClient();
 module.exports = dbclient;
